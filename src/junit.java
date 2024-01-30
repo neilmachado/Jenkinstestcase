2d9ec8d5b65509382ec150229f90cbf2ecd53763
Neil Machado <75448957+neilmachado@users.noreply.github.com>
2024-01-29 17:08:24 -0800

    Update junit.java

--- src/junit.java
+++ src/junit.java
@@ -1,43 +1,24 @@
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import static org.junit.Assert.*;
+package javatestcase;
 
-public class MathUtilsTest {
-    private MathUtils mathUtils;
+//MathUtils.java
+public class MathsUtils {
+ public int add(int a, int b) {
+     return a + b;
+ }
 
-    @Before
-    public void setUp() {
-        mathUtils = new MathUtils();
-    }
+ public int subtract(int a, int b) {
+     return a - b;
+ }
 
-    @Test
-    public void testAdd() {
-        assertEquals(5, mathUtils.add(2, 3));
-    }
+ public int multiply(int a, int b) {
+     return a * b;
+ }
 
-    @Test
-    public void testSubtract() {
-        assertEquals(2, mathUtils.subtract(5, 3));
-    }
-
-    @Test
-    public void testMultiply() {
-        assertEquals(15, mathUtils.multiply(3, 5));
-    }
-
-    @Test
-    public void testDivide() {
-        assertEquals(2.0, mathUtils.divide(6, 3), 0.001);
-    }
-
-    @Test
-    public void testDivideByZero() {
-        assertEquals(-1.0, mathUtils.divide(6, 0), 0.001);
-    }
-
-    @After
-    public void tearDown() {
-        mathUtils = null;
-    }
+ public double divide(int a, int b) {
+     if (b == 0) {
+         // Handle division by zero
+         return -1.0;
+     }
+     return (double) a / b;
+ }
 }
